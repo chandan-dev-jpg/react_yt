45249366a4bd6ebba1be782372a44641ef3b257c
Array in java <chandanbehera4598@gmail.com>
2026-06-12 23:10:50 +0530

    02_Components

---
+++ 02-componets/src/components/Card.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+const Card = () => {
+    let age=22;
+  return (
+    <div className="card">
+      <h1>Chandan Behera</h1>
+      <h3>i am a fullstack developer</h3>
+      <h3>my age is {age}</h3>
+    </div>
+  )
+}
+
+export default Card
