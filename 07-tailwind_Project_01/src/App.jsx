10969bb3c3a45ecd29d772be79a40a41683212d1
Array in java <chandanbehera4598@gmail.com>
2026-06-20 22:21:26 +0530

    tailwind Project

---
+++ 07-tailwind_Project_01/src/App.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+const App = () => {
+  return (
+    <div className="parent">
+    <div className="Right">
+
+    </div>
+    <div className="Left">
+
+    </div>
+    </div>
+  )
+}
+
+export default App
